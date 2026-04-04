347fbbee62d22798736d15b4e299524e1988ef0e
tavares-fiap <rm97877@fiap.com.br>
2026-04-03 23:53:37 -0300

    feat: wire redis, session_manager, and api_dispatcher in index.js

--- data_broker/index.js
+++ data_broker/index.js
@@ -1,14 +1,22 @@
+// data_broker/index.js
 const { loadBrokerConfig } = require('./config');
+const { createRedisClient } = require('./redis_client');
+const { createSessionManager } = require('./session_manager');
+const { createDispatcher } = require('./api_dispatcher');
 const { createHttpServer } = require('./http_server');
 const { createBrokerLogger } = require('./logger');
 const { createSocketServer, registerSocketHandlers } = require('./socket_handlers');
 
 const config = loadBrokerConfig();
 const log = createBrokerLogger();
-const server = createHttpServer();
+const redis = createRedisClient(config);
+const session = createSessionManager(redis, config, log);
+const dispatcher = createDispatcher(redis, config, log);
+const server = createHttpServer(session);
 const io = createSocketServer(server, config.allowedOrigins);
 
-registerSocketHandlers(io, log);
+registerSocketHandlers(io, log, session);
+dispatcher.start();
 
 server.listen(config.port, () => {
   log('info', 'broker_listening', {
